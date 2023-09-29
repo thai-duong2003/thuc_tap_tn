c90ae09216597dc398122b9ee5ad024e202573e6
thai-duong2003 <135040412+thai-duong2003@users.noreply.github.com>
2023-09-29 14:59:36 +0700

    Update routes. 4

--- src/Config/routes.js
+++ src/Config/routes.js
@@ -1,6 +1,6 @@
 const routes = {
-  home: "/thai-duong2003.github.io/thuc_tap_tn/",
-  nopage: "/thuc_tap_tn/*", //danh cho nhung link ko cos thif no se di vao trang nay
+  home: "/thuc_tap_tn/",
+  // nopage: "/thuc_tap_tn/*", //danh cho nhung link ko cos thif no se di vao trang nay
   following: "/thuc_tap_tn/following",
   profile: "/thuc_tap_tn/:nickname",
   live: "/thuc_tap_tn/live",
