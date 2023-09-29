9495cea77a3864e8be20b0afe2fa580b183eea03
Dương Quang Thai <thai211823@student.hactech.edu.vn>
2023-09-29 11:59:39 +0700

    up2

--- src/Config/routes.js
+++ src/Config/routes.js
@@ -2,7 +2,7 @@ const routes = {
   home: "/",
   nopage: "*", //danh cho nhung link ko cos thif no se di vao trang nay
   following: "/following",
-  profile: "/:nickname",
+  // profile: "/:nickname",
   live: "/live",
   khampha: "/explore",
   videodetail: "/videodetail",
