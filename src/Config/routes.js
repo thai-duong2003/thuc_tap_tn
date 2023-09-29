471da78e4fdfcf2a9b9a718426ec4b346d8e1e2d
Dương Quang Thai <thai211823@student.hactech.edu.vn>
2023-09-29 15:55:39 +0700

    update

--- src/Config/routes.js
+++ src/Config/routes.js
@@ -1,6 +1,6 @@
 const routes = {
   home: "/thuc_tap_tn",
-  // nopage: "/thuc_tap_tn/*", S//danh cho nhung link ko cos thif no se di vao trang nay
+  nopage: "/thuc_tap_tn/*", //danh cho nhung link ko cos thif no se di vao trang nay
   following: "/thuc_tap_tn/following",
   profile: "/thuc_tap_tn/:nickname",
   live: "/thuc_tap_tn/live",
